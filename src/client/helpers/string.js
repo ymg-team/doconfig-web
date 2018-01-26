e5d0031ead7c92595844226f44e8bce85d1345c0
yussan <yussandeveloper@gmail.com>
2018-01-26 23:01:12 +0700

    no reply

--- src/client/helpers/string.js
+++ src/client/helpers/string.js
@@ -2,10 +2,6 @@
  * function to convert text string to base64
  * @param {*} str
  */
-export function toBase64 (str) {
-  return str
-}
-
 export function nl2br (str) {
   return str.replace(/(?:\r\n|\r|\n)/g, '<br />')
 }
